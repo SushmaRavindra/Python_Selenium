d884c514d9968b84f8edbf3a03ed06d068475e5c
sandeepsuryaprasad <sandeep.suryaprasad@dxc.com>
2020-06-21 19:06:50 +0530

    testing

--- Training/Dependent_Independent.py
+++ Training/Dependent_Independent.py
@@ -1,7 +1,8 @@
-# Demo on Download Link of Custom Webpage
+# Demo on Download Link of Custom web page
 # Demo on getting Price Tag in http://demowebshop.tricentis.com/
 # Demo on Rate us option in http://demowebshop.tricentis.com/
 # Demo on Python Download link in python.org
 # Demo on Getting Phone Number of different branches of QSP
 # Demo on Show Batches link on QSP link for different courses
 # Demo on Getting Share Price on https://markets.businessinsider.com/
+# Demo on Selecting the checkbox Classic Selenium in http://www.qspiders.com/courses
