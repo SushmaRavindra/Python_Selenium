5b35a48f7a96110bad3f2f3f8af1df9dff77923d
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2020-08-27 09:04:07 +0530

    testing

--- Training/training_2.py
+++ Training/training_2.py
@@ -3,32 +3,51 @@ import time
 
 driver = webdriver.Chrome("./chromedriver")
 
-driver.get("file:///Users/sandeep/Documents/Python_Selenium/HTML_Pages/Tables.html")
+driver.get("http://demowebshop.tricentis.com/")
 
 time.sleep(3)
 
-driver.find_element_by_xpath("(//input[@type='checkbox'])[3]").click()
+driver.find_element_by_xpath("//a[text()='Register']").click()
+time.sleep(1)
+driver.find_element_by_xpath("//input[@value='M']").click()
+time.sleep(1)
+driver.find_element_by_xpath("//input[@name='FirstName']").send_keys("Sandeep")
+time.sleep(1)
+driver.find_element_by_xpath("//input[@id='LastName']").send_keys("Suryaprasad")
+time.sleep(1)
+driver.find_element_by_xpath("//input[@class='button-1 register-next-step-button']").click()
+
+driver.find_element_by_xpath("//input[@name='NewsletterEmail']").send_keys("abc@abc.com")
+for index in range(1, 5):
+    _xpath = f"//input[@id='pollanswers-{index}']"
+    driver.find_element_by_xpath(_xpath).click()
+    driver.find_element_by_xpath("//input[@value='Vote']").click()
+
+
 # General Form of CSS Selector
 # HTMLTAG[Property=value]
 
-# driver.find_element_by_css_selector("input[type='text']").send_keys("Sandeep")
-# driver.find_element_by_css_selector("input[type='password']").send_keys("Password123")
+# General form for xpath
+# HTML[@property=value]
+
+driver.find_element_by_css_selector("input[type='text']").send_keys("Sandeep")
+driver.find_element_by_css_selector("input[type='password']").send_keys("Password123")
 
 
-# driver.find_element_by_link_text("Register").click()
-#
-# time.sleep(1)
-#
-# driver.find_element_by_id("gender-male").click()
-#
-# time.sleep(1)
-#
-# driver.find_element_by_name("FirstName").send_keys("Sandeep")
-#
-# time.sleep(1)
-#
-# driver.find_element_by_id("LastName").send_keys("Suryaprasad")
-#
-# time.sleep(1)
-#
-# driver.find_element_by_name("register-button").click()
+driver.find_element_by_link_text("Register").click()
+
+time.sleep(1)
+
+driver.find_element_by_id("gender-male").click()
+
+time.sleep(1)
+
+driver.find_element_by_name("FirstName").send_keys("Sandeep")
+
+time.sleep(1)
+
+driver.find_element_by_id("LastName").send_keys("Suryaprasad")
+
+time.sleep(1)
+
+driver.find_element_by_name("register-button").click()
