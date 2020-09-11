159d7132cfe9e706f02da113085af9973305d0eb
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2020-09-11 11:23:09 +0530

    wait decorator

--- Training/training_2.py
+++ Training/training_2.py
@@ -7,44 +7,7 @@ import time
 
 
 driver = webdriver.Chrome('./chromedriver')
-driver.get("file:///Users/sandeep/Documents/Python_Selenium/HTML_Pages/Loading.html")
-
-# def enter_text(locator, *, value):
-#     locatortype, locatorvalue = locator
-#     driver.find_element(locatortype, locatorvalue).clear()
-#     driver.find_element(locatortype, locatorvalue).send_keys(value)
-#
-#
-
-# def click_element(locator):
-#     locatortype, locatorvalue = locator
-#     driver.find_element(locatortype, locatorvalue).click()
-#
-#
-# def select_item(locator, *, item):
-#     locatortype, locatorvalue = locator
-#     lst_box = driver.find_element(locatortype, locatorvalue)
-#     s = Select(lst_box)
-#     items = [opt.text for opt in s.options]
-#     if isinstance(item, str):
-#         if item not in items:
-#             raise ValueError(f'{item} not found in the list')
-#         s.select_by_visible_text(item)
-#     else:
-#         if item > len(items):
-#             raise IndexError('Index Error')
-#         s.select_by_index(item)
-#
-#
-# def select_items(locator, *, items):
-#     for item in items:
-#         try:
-#             select_item(locator, item=item)
-#             time.sleep(1)
-#         except (ValueError, IndexError):
-#             print(f'{item} was not found in the multi select listbox')
-#             continue
-
+driver.get("file:///Users/sandeep/Documents/Python_Selenium/HTML_Pages/Javascript_Alert.html")
 
 class element_visibility_and_enabled:
     def __init__(self, locator):
@@ -59,35 +22,104 @@ class element_visibility_and_enabled:
             return False
 
 
-def element_visibility_enable(locator):
+def element_to_be_visible_enabled(locator):
     def wrapper(driver):
         loctype, locvalue = locator
         element = driver.find_element(loctype, locvalue)
-        if element.is_enabled() and element.is_displayed():
+        if element.is_displayed() and element.is_enabled():
             return True
         else:
             return False
     return wrapper
 
 
-def wait_element_to_be_present(locator, item):
-    def wrapper(driver):
-        loctype, locvalue = locator
-        element = driver.find_element(loctype, locvalue)
-        s = Select(element)
-        items = [opt.text for opt in s.options]
+def _wait(*, visible=True, enabled=True, max_timeout=10, is_alert=False):
+    def decorate(func):
+        w = WebDriverWait(driver, max_timeout)
+        def wrapper(*args, **kwargs):
+            locator, = args
+            if visible and enabled and not is_alert:
+                w.until(element_visibility_and_enabled(locator))
+            elif visible and not enabled and not is_alert:
+                w.until(ec.visibility_of_element_located(locator))
+            elif not visible and not is_alert:
+                w.until(ec.invisibility_of_element_located(locator))
+            elif is_alert:
+                w.until(ec.alert_is_present)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorate
+
+
+@_wait(max_timeout=3)
+def enter_text(locator, *, value):
+    locatortype, locatorvalue = locator
+    driver.find_element(locatortype, locatorvalue).clear()
+    driver.find_element(locatortype, locatorvalue).send_keys(value)
+
+
+@_wait(is_alert=True)
+def click_element(locator):
+    locatortype, locatorvalue = locator
+    driver.find_element(locatortype, locatorvalue).click()
+
+
+@_wait(max_timeout=8)
+def select_item(locator, *, item):
+    locatortype, locatorvalue = locator
+    lst_box = driver.find_element(locatortype, locatorvalue)
+    s = Select(lst_box)
+    items = [opt.text for opt in s.options]
+    if isinstance(item, str):
         if item not in items:
-            return False
-        else:
-            return True
-    return wrapper
+            raise ValueError(f'{item} not found in the list')
+        s.select_by_visible_text(item)
+    else:
+        if item > len(items):
+            raise IndexError('Index Error')
+        s.select_by_index(item)
 
 
-w = WebDriverWait(driver, 15)
+@_wait()
+def select_items(locator, *, items):
+    for item in items:
+        try:
+            select_item(locator, item=item)
+            time.sleep(1)
+        except (ValueError, IndexError):
+            print(f'{item} was not found in the multi select listbox')
+            continue
+
+
+# click_element(('xpath', '//a'))
+# enter_text(("name", "fname"), value="Hello")
+# click_element(("xpath", "//a[text()='Register']"))
+# click_element(('id', 'gender-male'))
+# enter_text(("name", "FirstName"), value="Sandeep")
+# enter_text(("name", "LastName"), value="Suryaprasad")
+# click_element(("id", "register-button"))
+
+
+
+
+# def element_visibility_enable(locator):
+#     def wrapper(*args, **kwargs):
+#         loctype, locvalue = locator
+#         element = driver.find_element(loctype, locvalue)
+#         s = Select(element)
+#         items = [opt.text for opt in s.options]
+#         if item not in items:
+#             return False
+#         else:
+#             return True
+#     return wrapper
+
+
+# w = WebDriverWait(driver, 15)
 # s = element_visibility_and_enabled(("name", "fname"))
-s = element_visibility_enable(("name", "fname"))
-w.until(s)
-driver.find_element_by_name("fname").send_keys("Hello")
+# s = element_to_be_visible_enabled(("name", "fname"))
+# w.until(s)
+# driver.find_element_by_name("fname").send_keys("Hello")
 
 
 
