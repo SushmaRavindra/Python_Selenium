8cad51807724d658ec554c62a5f0c55451779a85
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2020-10-04 08:13:30 +0530

    excel

--- Training/reading_excel.py
+++ Training/reading_excel.py
@@ -1,32 +1,9 @@
 import xlrd
 
-def read_locators(sheetname):
-    wb = xlrd.open_workbook('./Objects.xlsx')
-    ws = wb.sheet_by_name(sheetname)
-    rows = ws.get_rows()
-    headers = next(rows)    # Skip the headers
-    data = {row[0].value: (row[1].value, row[2].value) for row in rows}
-    return data
 
+wb = xlrd.open_workbook('./sample.xlsx')
+ws = wb.sheet_by_name('Covid')
+rows = ws.get_rows()
 
-wb = xlrd.open_workbook('./Objects.xlsx')
-ws = wb.sheet_by_name("RegistrationPage")
-print(ws.row_values(4, start_colx=1))
-
-
-rp = read_locators("RegistrationPage")
-
-lp = read_locators("LoginPage")
-
-print(rp)
-print(lp)
-
-
-def enter_email():
-    print(lp['txt_email'])
-
-def enter_password():
-    print(lp['txt_password'])
-
-enter_email()
-enter_password()
+for row in rows:
+    print(row)
