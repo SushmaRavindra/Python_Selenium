879bdd581e4c7f3dea6865cb1a94aceeac364b6d
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2020-10-14 20:05:02 +0530

    select

--- QPYE2/_browser_commands.py
+++ QPYE2/_browser_commands.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.support.select import Select
+from selenium.common.exceptions import NoSuchElementException
 
 
 driver = webdriver.Chrome('./chromedriver')
-driver.get('http://demowebshop.tricentis.com')
-
+driver.get('file:///Users/sandeep/Documents/Python_Selenium/HTML_Pages/Standard_listbox.html')
+sleep(2)
 
 def enter_text(locator, *, value):
     loctype, locvalue = locator
@@ -19,31 +20,71 @@ def click_element(locator):
 
 
 def select_item(locator, *, item):
-    ...
+    loctype, locvalue = locator
+    lst_box = driver.find_element(loctype, locvalue)
+    s = Select(lst_box)
+    opts = s.options
+    items = [opt.text for opt in opts]
+    if isinstance(item ,str):
+##        if not item in items:
+##            raise ValueError(f'{item} not found in the listbox')
+        s.select_by_visible_text(item)
+    elif isinstance(item, int):
+##        if item > len(items)-1:
+##            raise ValueError(f'Item with {item} does not exist')
+        s.select_by_index(item)
+    else:
+        raise TypeError
+
+def select_multiple_items(locator, *, items):
+    for item in items:
+        try:
+            select_item(locator, item=item)            
+        except NoSuchElementException:
+            print(f'{item} not found in the listbox')
+            continue
+    
 
 
-click_element(("xpath", "//a[text()='Register']"))
-sleep(2)
+select_multiple_items(("id", "multiple_cars"), items=["Audi", "Maruti", "Honda", "Ford", 3, 5])
+##sleep(2)
+##
+##select_item(("id", "standard_cars"), item=8)
+##sleep(2)
+##
+##select_item(("id", "standard_cars"), item="Jaguar")
+##sleep(2)
+##
+##select_item(("id", "standard_cars"), item=14)
+##sleep(2)
 
-click_element(("id", "gender-male"))
-sleep(2)
+##select_item(("id", "standard_cars"), item="Maruti")
 
-enter_text(("name", "FirstName"), value="Steve")
-sleep(2)
 
-enter_text(("id", "LastName"), value="Jobs")
-sleep(2)
 
-enter_text(("id", "Email"), value="steve.jobs@company.com")
-sleep(2)
 
-enter_text(("name", "Password"), value="password123")
-sleep(2)
-
-enter_text(("name", "ConfirmPassword"), value="password123")
-sleep(2)
-
-click_element(("name", "register-button"))
+##click_element(("xpath", "//a[text()='Register']"))
+##sleep(2)
+##
+##click_element(("id", "gender-male"))
+##sleep(2)
+##
+##enter_text(("name", "FirstName"), value="Steve")
+##sleep(2)
+##
+##enter_text(("id", "LastName"), value="Jobs")
+##sleep(2)
+##
+##enter_text(("id", "Email"), value="steve.jobs@company.com")
+##sleep(2)
+##
+##enter_text(("name", "Password"), value="password123")
+##sleep(2)
+##
+##enter_text(("name", "ConfirmPassword"), value="password123")
+##sleep(2)
+##
+##click_element(("name", "register-button"))
 
 
 
