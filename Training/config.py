09a78293ac9c8b4797ac8879ad796c5b1a0367bb
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2020-11-08 14:56:53 +0530

    testing

--- Training/config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    URL = "http://demowebshop.tricentis.com/"
-    CHROME_DRIVER_PATH = "./chromedriver"
-    FIREFOX_DRIVER_PATH = "./geckodriver"
-    MAX_TIME_OUT = 30
-    USER_NAME = "sandeep"
-    PASSWORD = "Password123"
